a112596991e19da9e881d216bfc71c3fee803cf0
Dawid Urbas <durbas92@gmail.com>
2025-09-05 12:20:01 +0200

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TonConnectUIProvider manifestUrl={`${window.location.origin}/tonconnect-manifest.json`}>
+    <TonConnectUIProvider 
+      // For demo purposes, we're using a static manifest URL
+      // Replace with your own: manifestUrl={`${window.location.origin}/tonconnect-manifest.json`}
+      manifestUrl="https://gist.githubusercontent.com/mrruby/243180339f492a052aefc7a666cb14ee/raw/">
       <App />
     </TonConnectUIProvider>
   </StrictMode>,
